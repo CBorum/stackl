7edf25210b885cfa46bd7430cb8133962918ae80
Christopher Borum <christopherborum@gmail.com>
2019-10-13 13:49:37 +0200

    Create test-queries.sql

---
+++ test-queries.sql
@@ -0,0 +1,7 @@
+insert into stackl_user (username, password) values ('raw2', 'raw2password');
+
+insert into marking (user_id, row_id, table_name) values (1, 1258694, 'post')
+
+insert into marking (user_id, row_id, table_name) values (1, 2782235, 'comment')
+
+select marker(1, 2782235, 'comments' , 'hvornår er kaffe og drømmekage')
