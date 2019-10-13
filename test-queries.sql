25af60b4f03573724a434ddc70f5bf1727b6a1db
Magnus Larsen <mal@pernexus.dk>
2019-10-13 16:13:22 +0200

    tilføjet ids til test af funcs

--- test-queries.sql
+++ test-queries.sql
@@ -10,18 +10,18 @@ select searcher(1, 'injection');
 
 select * from search_entry where user_id = 1;
 
-select * from exact_match('sql injection');
+select * from exact_match(1, 'sql injection');
 
-select * from best_match('sql','injection');
+select * from best_match(1, 'sql','injection');
 
-select * from best_match_w_body('sql','injection');
+select * from best_match_w_body(1, 'sql','injection');
 
-select * from ranked_weighted_2('sql','injection');
+select * from ranked_weighted_2(1, 'sql','injection');
 
-select * from ranked_weighted_2_w_body('sql','injection');
+select * from ranked_weighted_2_w_body(1, 'sql','injection');
 
-select * from words_to_words('sql injection');
+select * from words_to_words(1, 'sql injection');
 
-select * from words_to_words_weighted_avg('sql injection');
+select * from words_to_words_weighted_avg(1, 'sql injection');
 
-select * from words_to_words_weighted_sum('sql injection');
+select * from words_to_words_weighted_sum(1, 'sql injection');
