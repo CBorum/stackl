355f6b287b587697650a1bac3ddf94fc032641af
Magnus Larsen <mal@pernexus.dk>
2019-10-13 17:04:42 +0200

    tilføjet select før og efter

--- test-queries.sql
+++ test-queries.sql
@@ -1,16 +1,14 @@
 insert into stackl_user (username, password) values ('raw2', 'raw2password');
 
-insert into marking (user_id, row_id, table_name) values (1, 1258694, 'post');
-
-insert into marking (user_id, row_id, table_name) values (1, 2782235, 'comment');
-
 select marker(1, 9158957, 'comment', null);
 
 select marker(1, 1252, 'post', 'Meget brugbar post');
 
+select * from search_entry;
+
 select searcher(1, 'injection');
 
-select * from search_entry where user_id = 1;
+select * from search_entry;
 
 select * from exact_match(1, 'sql injection');
 
