ccc3b9f75459d771c7988de093f8016eff50dab2
kristian <kristian@kogn.lan>
2019-11-05 12:43:46 +0100

    Env.cs remade with dictionary

--- Env.cs
+++ Env.cs
@@ -9,16 +9,16 @@ namespace stackl {
     class Env {
         private static Env instance {get;} = new Env();
 
-        public List<KeyValuePair<String, String>> pairs {get;}
+        public Dictionary<String, String> Dict {get;}
 
         private Env(){
             try{
-                pairs = new List<KeyValuePair<string, string>>();
+                Dict = new Dictionary<string, string>();
                 string text = System.IO.File.ReadAllText(".env");
                 var lines = text.Split(@"\r");
                 foreach(var line in lines){
                     var splitLine = line.Split("=", 2);
-                    pairs.Add(new KeyValuePair<string, string>(splitLine[0].Trim(), splitLine[1].Trim()));
+                    Dict[splitLine[0].Trim()] = splitLine[1].Trim();
                 }
             }catch(Exception ex){
                 Console.WriteLine("Failed to read .env file (is a .env file present in the project directory?)");
