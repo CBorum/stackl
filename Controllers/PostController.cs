36584d063470b650a835a00af595aece0308dfe3
Kristoffer Noga <k.noga@hotmail.com>
2019-11-07 10:32:26 +0100

    minor change

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,14 +1,24 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using stackl.DataAccessLayer;
+using stackl.Models;
 
 namespace stackl.Controllers {
     [ApiController]
     [Route("api/[controller]")]
     public class PostController : ControllerBase {
 
-        
-        
+        [HttpGet("{id}")]
+        public ActionResult GetPost(int id)
+        {
+            System.Console.WriteLine("HALLLLLOOOOOOOOO, " + this.Request.ContentType.Equals("application/json"));
+            var p = new Post
+            {
+                PostId = 1,
+                Body = "bra teste"
+            };
+            return Ok(p);
+        }
 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -36,11 +36,11 @@ namespace stackl
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
