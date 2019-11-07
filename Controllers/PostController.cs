699d36e0bf418f8020f7e8322d7bc447e22a6526
Kristoffer Noga <k.noga@hotmail.com>
2019-11-07 13:59:35 +0100

    mere fix

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -8,26 +8,12 @@ namespace stackl.Controllers {
     [Route("api/[controller]")]
     public class PostController : ControllerBase {
 
-<<<<<<< HEAD
-        [HttpGet("{id}")]
-        public ActionResult GetPost(int id)
-        {
-            System.Console.WriteLine("HALLLLLOOOOOOOOO, " + this.Request.ContentType.Equals("application/json"));
-            var p = new Post
-            {
-                PostId = 1,
-                Body = "bra teste"
-            };
-            return Ok(p);
-        }
-=======
         [HttpGet("{id}", Name = nameof(GetPost))]
         public ActionResult GetPost(int id)
         {
             return Ok();
         }
         
->>>>>>> cd681cd7e7689317c70e556f769b86d2f2d3210c
 
     }
 }
