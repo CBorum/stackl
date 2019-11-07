06d1ad1abc1a807ef4293b0633b9c69240744603
Magnus Larsen <mal@pernexus.dk>
2019-11-07 11:39:28 +0100

    Tilføjet search tests

---
+++ Tests/SearchDataServiceTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using stackl.DataAccessLayer;
+using stackl.Models;
+using Xunit;
+
+namespace stackl.Tests
+{
+    public class SearchDataServiceTests
+    {
+        [Fact]
+        public void TestSearchByID_NotFound()
+        {
+            //Given
+            SearchDataService searchDataService = new SearchDataService();
+            
+            //When
+            var posts = searchDataService.RankedWeightedSearch(-1, 0, 10, "test");
+
+            //Then
+            Assert.Null(posts);
+        }
+        [Fact]
+        public void TestSearchByLimit_10()
+        {
+            //Given
+            SearchDataService searchDataService = new SearchDataService();
+            
+            //When
+            var posts = searchDataService.RankedWeightedSearch(1, 0, 10, "java");
+            //Then
+            Assert.Equal(10, posts.Count);
+        }
+    }
+}
