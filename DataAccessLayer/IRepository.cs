fa99859b9a5d946d1ec5f497c10a4f3f7c47cd05
Magnus Larsen <mal@pernexus.dk>
2019-11-07 09:32:20 +0100

    Fjernet prohibiterede public fra interface

--- DataAccessLayer/IRepository.cs
+++ DataAccessLayer/IRepository.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 namespace stackl {
     public interface IRepository<T> {
-        public T Get(int id);
-        public List<T> GetAll();
-        public T Create(T entity);
-        public T Update(T entity);
-        public Boolean Delete(T entity);
+        T Get(int id);
+        List<T> GetAll();
+        T Create(T entity);
+        T Update(T entity);
+        Boolean Delete(T entity);
     }
 }
