777ec7988a108b0e93381e136824561444b3b08b
kristian <kristian.o.g.nielsen@gmail.com>
2019-12-07 16:38:35 +0100

    sidebar markup sat op, login/register virker, markings og search history mangler api kald og render

--- stackl-frontend/src/components/modals/LoginModal.js
+++ stackl-frontend/src/components/modals/LoginModal.js
@@ -8,11 +8,14 @@ const makeToast = (msg, options = {}) => {
     alert(msg);
 };
 
+const usernameInputId = 'modal_username';
+const passwordInputId = 'modal_password';
+
 const unsafeGetInputValue = query => (document.querySelector(query) || {}).value;
 
 const unsafeLogin = () => {
-    const username = unsafeGetInputValue('#username');
-    const password = unsafeGetInputValue('#password');
+    const username = unsafeGetInputValue(`#${usernameInputId}`);
+    const password = unsafeGetInputValue(`#${passwordInputId}`);
 
     if(!username || !password){
         makeToast('Username or password missing');
@@ -27,12 +30,12 @@ function LoginModal(props){
         <ModalWrapper modalId={MODAL_IDS.LOGIN}>
             <h1 className="display-4 text-center mb-5">Login</h1>
             <div className="form-group mb-3 mw-450 mx-auto">
-                <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" id="username" placeholder="Enter username"/>
+                <label htmlFor={usernameInputId}>Username</label>
+                <input type="text" className="form-control" id={usernameInputId} placeholder="Enter username"/>
             </div>
             <div className="form-group mb-5 mw-450 mx-auto">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" placeholder="Enter password"/>
+                <label htmlFor={passwordInputId}>Password</label>
+                <input type="password" className="form-control" id={passwordInputId} placeholder="Enter password"/>
             </div>
             <button onClick={unsafeLogin} className="btn btn-primary btn-block mw-450 mx-auto">Submit</button>
         </ModalWrapper>
--- stackl-frontend/src/middleware/index.js
+++ stackl-frontend/src/middleware/index.js
@@ -2,11 +2,13 @@ import { applyMiddleware } from 'redux'
 import TestMiddleware from './TestMiddleware'
 import PostMiddleware from './PostMiddleware'
 import LoginMiddleware from './LoginMiddleware'
+import RegisterMiddleware from './RegisterMiddleware'
 
 const middleware = applyMiddleware(
     TestMiddleware,
     PostMiddleware,
-    LoginMiddleware
+    LoginMiddleware,
+    RegisterMiddleware
 );
 
 export default middleware
