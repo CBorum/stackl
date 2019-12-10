0805c85eba06cf9c22a2b0326739bb8893279f6c
kristian <kristian@kogn.lan>
2019-12-10 11:26:57 +0100

    register modal now works

--- stackl-frontend/src/components/modals/RegisterModal.js
+++ stackl-frontend/src/components/modals/RegisterModal.js
@@ -1,35 +1,49 @@
 import React from "react";
 import ModalWrapper from './ModalWrapper';
 import {MODAL_IDS} from '../../actions/ModalActions';
+import store from "../../store";
+import {register} from "../../actions/RegisterActions";
 
-const invalidClass = 'invalid';
-
-
-
-const validateElement = validator => event => {
-    if(!validator(event.target.value)){
-        event.target.classList.add(invalidClass)
-    }else{
-        event.target.classList.remove(invalidClass)
-    }
+const makeToast = (msg, options = {}) => {
+    alert(msg);
 };
 
-const validateUsernameElement = validateElement(username => typeof username === 'string' && username > 4);
-const validatePasswordElement = validateElement(password => typeof password === 'string' && password > 4);
+const usernameInputId = 'modal_new_username';
+const passwordInputId = 'modal_new_password';
+
+const unsafeGetInputValue = query => (document.querySelector(query) || {}).value;
+
+const unsafeRegister = () => {
+    const username = unsafeGetInputValue(`#${usernameInputId}`);
+    const password = unsafeGetInputValue(`#${passwordInputId}`);
+
+    if(!username || !password){
+        makeToast('Username or password missing');
+        return;
+    }
+
+    store.dispatch(register(username, password));
+};
+
+const onEnter = handler => event => {
+    if (event.key === 'Enter') {
+        handler();
+    }
+};
 
 function RegisterModal() {
     return (
         <ModalWrapper modalId={MODAL_IDS.REGISTER}>
             <h1 className="display-4 text-center mb-5">Register</h1>
             <div className="form-group mb-3 mw-450 mx-auto">
-                <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username"/>
+                <label htmlFor={usernameInputId}>Username</label>
+                <input type="text" className="form-control" id={usernameInputId} aria-describedby="emailHelp" placeholder="Enter username"/>
             </div>
             <div className="form-group mb-5 mw-450 mx-auto">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter password"/>
+                <label htmlFor={passwordInputId}>Password</label>
+                <input onKeyPress={onEnter(unsafeRegister)} type="password" className="form-control" id={passwordInputId} aria-describedby="emailHelp" placeholder="Enter password"/>
             </div>
-            <button className="btn btn-primary btn-block mw-450 mx-auto">Submit</button>
+            <button onClick={unsafeRegister} className="btn btn-primary btn-block mw-450 mx-auto">Submit</button>
         </ModalWrapper>
     );
 }
