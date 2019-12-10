4fa4498ad3adc169ca7131becd8343113d44dfbe
kristian <kristian@kogn.lan>
2019-12-10 10:56:53 +0100

    register api call now returns nocontent (204)

--- stackl-frontend/src/middleware/apiService.js
+++ stackl-frontend/src/middleware/apiService.js
@@ -22,6 +22,14 @@ const port = process.env.REACT_APP_STACKL_API_PORT || 5000;
 
 export const getAddress = (endpoint) => `${host}:${port}/${endpoint}`;
 
+export const sleepAsync = ms => {
+	store.dispatch(startLoad());
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+		store.dispatch(endLoad())
+	});
+};
+
 export const apiCall = (dispatch, endpoint, method, data) => {
 	dispatch(startLoad());
 	return fetch(getAddress(endpoint), {
@@ -29,20 +37,31 @@ export const apiCall = (dispatch, endpoint, method, data) => {
 		method: method,
 		body: JSON.stringify(data)
 	}).then((res) => {
+		console.log(1);
 		if (res.status === 403) {
 			dispatch(logout());
 			return;
 		}
+		console.log(2);
 
-		if (res.ok) {
-			try{
-                return res.json()
-			}catch(err){
-				return res.text();
-			}
-
-		} else {
+		if(!res.ok){
 			throw Error(res.statusText)
+		}
+
+		console.log(3);
+		return res.text()
+	}).then(responseText => {
+		console.log(4);
+
+		if(responseText){
+			return;
+		}
+		console.log(5);
+
+		try{
+			return JSON.parse(responseText);
+		}catch(err){
+			return responseText
 		}
 	}).catch((e) => {
 		throw Error(e)
