758c6126bff4e139fbe331f1790e44b87ff79843
kristian <kristian.o.g.nielsen@gmail.com>
2019-12-11 14:35:38 +0100

    styling

--- stackl-frontend/src/components/modals/ModalWrapper.js
+++ stackl-frontend/src/components/modals/ModalWrapper.js
@@ -7,7 +7,7 @@ const mapStateToProps = state => ({currentModalId: state.Modal.currentModalId});
 
 function ModalWrapper(props) {
     return (
-        <div className={`modal-container pt-5 ${props.currentModalId === props.modalId ? 'display' : ''}`}>
+        <div className={`modal-container pt-5 px-3 ${props.currentModalId === props.modalId ? 'display' : ''}`}>
             <div onClick={props.dispatch.bind(null, hideCurrentModal())} className="modal-close-btn">close</div>
             {props.children}
         </div>
