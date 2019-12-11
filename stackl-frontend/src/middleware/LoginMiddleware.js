5476da66be264688c1d81eb52467b26f5754257f
kristian <kristian.o.g.nielsen@gmail.com>
2019-12-11 10:09:28 +0100

    saved posts now retrieved on refresh

--- stackl-frontend/src/middleware/LoginMiddleware.js
+++ stackl-frontend/src/middleware/LoginMiddleware.js
@@ -1,4 +1,4 @@
-import { LOGIN } from "../actions/ActionTypes";
+import { LOGIN, LOGIN_DONE } from "../actions/ActionTypes";
 import { loginDone } from '../actions/LoginActions';
 import { hideModal, MODAL_IDS } from '../actions/ModalActions';
 import { getSavedPosts } from '../actions/SavedPostsActions';
@@ -11,12 +11,14 @@ const LoginMiddleware = ({ dispatch, getState }) => (next) => (action) => {
             apiCall(dispatch, `api/login/authenticate`, 'POST', action.payload)
                 .then(res => {
                     dispatch(loginDone(res));
-                    dispatch(hideModal(MODAL_IDS.LOGIN));
-                    dispatch(getSavedPosts());
                 })
                 .catch(e => {
                     console.log("error: " + e)
                 });
+            break;
+        case LOGIN_DONE:
+            dispatch(hideModal(MODAL_IDS.LOGIN));
+            dispatch(getSavedPosts());
             break;
         default:
             break;
